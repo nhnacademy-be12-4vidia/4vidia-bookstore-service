d443c685735dffff5e45921bffc61e4709df800a
Link <mexlove2@gmail.com>
2025-11-23 13:37:34 +0900

    feat: Dto -> Entity 매서드 구현 및 책 엔티티 빌더 필드 추가
    - 서비스 코드 단순화를 위해 Dto에서 엔티티로 변환하는 매서드를 Dto에 생성함

---
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/exception/BookAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy._vidiabookstoreservice.book.exception;
+
+public class BookAlreadyExistsException extends RuntimeException {
+  public BookAlreadyExistsException(String message) {
+    super(message);
+  }
+}
