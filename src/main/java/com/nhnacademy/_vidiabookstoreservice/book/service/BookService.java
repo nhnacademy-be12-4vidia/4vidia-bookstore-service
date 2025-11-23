4e3fd03b09243a2ca5e09fa0c9206b0b9b554f94
Link <mexlove2@gmail.com>
2025-11-23 18:36:25 +0900

    rename: 폴더 이동으로 수정 - 수정

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/book/service/BookService.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/service/BookService.java
@@ -1,10 +1,10 @@
 package com.nhnacademy._vidiabookstoreservice.book.service;
 
-import com.nhnacademy._vidiabookstoreservice.book.dto.BookCreateRequest;
-import com.nhnacademy._vidiabookstoreservice.book.dto.BookIdDto;
+import com.nhnacademy._vidiabookstoreservice.book.dto.request.BookCreateRequest;
+import com.nhnacademy._vidiabookstoreservice.book.dto.response.BookIdResponse;
 
 public interface BookService {
 
-    BookIdDto createBook(BookCreateRequest request);
+    BookIdResponse createBook(BookCreateRequest request);
 
 }
