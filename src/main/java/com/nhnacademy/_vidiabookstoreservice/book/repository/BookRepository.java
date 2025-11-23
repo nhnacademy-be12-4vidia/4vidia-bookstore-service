41f002efd1e39636ea38db3a81d1e0d76c876960
Link <mexlove2@gmail.com>
2025-11-23 18:37:13 +0900

    feat: 도서 조회시 도서 작가 fetch join - fetch join 구현

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/book/repository/BookRepository.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/repository/BookRepository.java
@@ -1,9 +1,15 @@
 package com.nhnacademy._vidiabookstoreservice.book.repository;
 
 import com.nhnacademy._vidiabookstoreservice.book.domain.Book;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     boolean existsByIsbn(String isbn);
+
+    @Query("SELECT b FROM Book b JOIN FETCH b.bookAuthors ba JOIN FETCH ba.author WHERE b.id = :bookId")
+    Optional<Book> findByIdWithAuthors(@Param("bookId") Long bookId);
 }
