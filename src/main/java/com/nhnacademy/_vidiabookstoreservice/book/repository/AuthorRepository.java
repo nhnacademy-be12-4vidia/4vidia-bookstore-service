5512bc5c2cbff7af4e766c1c9ebde79008415f02
Link <mexlove2@gmail.com>
2025-11-24 17:31:12 +0900

    feat: 작가 CR - 작가 생성, 조회 CR 구현

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/book/repository/AuthorRepository.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/repository/AuthorRepository.java
@@ -2,9 +2,15 @@ package com.nhnacademy._vidiabookstoreservice.book.repository;
 
 import com.nhnacademy._vidiabookstoreservice.book.domain.Author;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     Optional<Author> findByName(String name);
+
+    Page<Author> findByNameContaining(String keyword, Pageable pageable);
+
+    boolean existsByName(String name);
 }
