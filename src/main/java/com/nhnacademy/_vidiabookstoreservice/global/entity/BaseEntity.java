842e9d884f580a543fff6d78c1f45b3e61896787
Link <mexlove2@gmail.com>
2025-11-26 16:24:10 +0900

    feat: BaseEntity - BaseEntity 생성일, 수정일 내용 추가

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/global/entity/BaseEntity.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/global/entity/BaseEntity.java
@@ -1,4 +1,24 @@
 package com.nhnacademy._vidiabookstoreservice.global.entity;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
