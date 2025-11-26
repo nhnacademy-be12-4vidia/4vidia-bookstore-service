d94960be16ec6d57b06a867f834bf499db44e781
Link <mexlove2@gmail.com>
2025-11-26 09:37:18 +0900

    test: MINIO test - MINIO에 정상 업로드 되는지 확인하는 테스트코드 작성

---
+++ src/test/java/com/nhnacademy/_vidiabookstoreservice/book/service/impl/MinioServiceTest.java
@@ -0,0 +1,48 @@
+package com.nhnacademy._vidiabookstoreservice.book.service.impl;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.nhnacademy._vidiabookstoreservice.order.controller.PaymentController;
+import com.nhnacademy._vidiabookstoreservice.order.service.TossPaymentService;
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.web.multipart.MultipartFile;
+
+@SpringBootTest
+@Slf4j
+class MinioServiceTest {
+
+    @Autowired
+    private MinioService minioService;
+
+    @MockitoBean
+    private PaymentController paymentController;
+
+    @MockitoBean
+    private TossPaymentService tossPaymentService;
+
+    @Test
+    void uploadRealFile() {
+
+        MultipartFile file = new MockMultipartFile(
+            "image",
+            "test-image.jpg",
+            "image/jpeg",
+            "Hello Minio".getBytes()
+        );
+
+        String url = minioService.upload(file);
+
+        log.info("업로드 Url : {}", url);
+
+        assertThat(url).isNotNull();
+        assertThat(url).contains("test-image.jpg");
+    }
+
+}
