44c974221c88696e7b9d1a7fdfacc5f39e829f49
Link <mexlove2@gmail.com>
2025-11-26 16:27:42 +0900

    feat: 카테고리 서비스 선언, 구현 - 카테고리 목록 조회와 서비스 내부 호출용 카네고리 엔티티 반환 매서드 구현

---
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.nhnacademy._vidiabookstoreservice.book.service.impl;
+
+import com.nhnacademy._vidiabookstoreservice.book.domain.Category;
+import com.nhnacademy._vidiabookstoreservice.book.dto.category.response.CategoryListResponse;
+import com.nhnacademy._vidiabookstoreservice.book.exception.CategoryNotFoundException;
+import com.nhnacademy._vidiabookstoreservice.book.repository.CategoryRepository;
+import com.nhnacademy._vidiabookstoreservice.book.service.CategoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<CategoryListResponse> getCategoryList() {
+
+        List<Category> categoryList = categoryRepository.findAll();
+        return categoryList.stream().map(CategoryListResponse::from).toList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Category getCategory(Long categoryId) {
+        return categoryRepository.findById(categoryId).orElseThrow(
+            () -> new CategoryNotFoundException(
+                "해당하는 아이디의 카테고리는 존재하지 않습니다. ID: %d".formatted(categoryId)));
+    }
+}
