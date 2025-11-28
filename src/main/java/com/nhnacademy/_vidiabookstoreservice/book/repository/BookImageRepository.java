4152a0e9b9d22e35cf4a00c31c46a16ed69eed06
Link <mexlove2@gmail.com>
2025-11-28 18:38:41 +0900

    refactor: 책 이미지 전체 등록 수정 - 중복 데이터는 제거하고 등록하게 수정

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/book/repository/BookImageRepository.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/repository/BookImageRepository.java
@@ -1,11 +1,16 @@
 package com.nhnacademy._vidiabookstoreservice.book.repository;
 
-import com.nhnacademy._vidiabookstoreservice.book.domain.Book;
 import com.nhnacademy._vidiabookstoreservice.book.domain.BookImage;
+import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookImageRepository extends JpaRepository<BookImage, Long> {
 
-    boolean existsByBookIdAndImageUrl(Long bookId, String imageUrl);
+    boolean existsByBook_IdAndImageUrl(Long bookId, String imageUrl);
 
+    @Query("SELECT bi.imageUrl FROM BookImage bi WHERE bi.imageUrl IN : urls")
+    Set<String> findExistingUrls(@Param("urls") List<String> urls);
 }
