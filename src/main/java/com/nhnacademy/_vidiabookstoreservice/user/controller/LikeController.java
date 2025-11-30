f4643ac12b826774c65d63a08f20a0cdc4b2d5d3
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-11-30 23:42:09 +0900

    feat: [회원] 좋아요 기능 추가
    - 좋아요 등록/삭제 및 좋아요리스트조회 기능 추가

---
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/user/controller/LikeController.java
@@ -0,0 +1,44 @@
+package com.nhnacademy._vidiabookstoreservice.user.controller;
+
+import com.nhnacademy._vidiabookstoreservice.user.dto.response.LikeResponse;
+import com.nhnacademy._vidiabookstoreservice.user.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/my/likes")
+public class LikeController {
+    private final LikeService likeService;
+
+    /**
+     * 좋아요 리스트 조회
+     */
+    @GetMapping
+    public ResponseEntity<List<LikeResponse>> getLikeList(@RequestHeader("X-USER-ID") Long userId){
+        return ResponseEntity.ok(likeService.getLikes(userId));
+    }
+
+    /**
+     * 좋아요 등록
+     */
+    @PostMapping("/{bookId}")
+    public ResponseEntity<String> addLike(@RequestHeader("X-USER-ID") Long userId,
+                                          @PathVariable Long bookId) {
+        likeService.addLike(userId, bookId);
+        return ResponseEntity.ok("리뷰등록완");
+    }
+
+    /**
+     * 좋아요 삭제
+     */
+    @DeleteMapping("/{bookId}")
+    public ResponseEntity<String> removeLike(@RequestHeader("X-USER-ID") Long userId,
+                                             @PathVariable Long bookId) {
+        likeService.removeLike(userId, bookId);
+        return ResponseEntity.ok("리뷰삭제완");
+    }
+}
---
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/user/domain/Like.java
@@ -0,0 +1,34 @@
+package com.nhnacademy._vidiabookstoreservice.user.domain;
+
+import com.nhnacademy._vidiabookstoreservice.book.domain.Book;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "likes", // 'like' = sql 예약어
+        uniqueConstraints = @UniqueConstraint(name = "uk_user_book", columnNames = {"user_id", "book_id"})
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id", nullable = false)
+    private Long likeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+
+    @Builder
+    public Like(User user, Book book) {
+        this.user = user;
+        this.book = book;
+    }
+}
