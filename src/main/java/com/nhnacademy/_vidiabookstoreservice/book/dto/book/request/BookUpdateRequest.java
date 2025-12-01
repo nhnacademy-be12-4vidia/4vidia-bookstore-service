0ad2922e487b3ff365636f2946bdca04b284addf
Link <mexlove2@gmail.com>
2025-12-01 19:31:55 +0900

    feat: 도서 수정 요청 생성 - 도서 수정 요청 생성

---
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/dto/book/request/BookUpdateRequest.java
@@ -0,0 +1,54 @@
+package com.nhnacademy._vidiabookstoreservice.book.dto.book.request;
+
+import com.nhnacademy._vidiabookstoreservice.book.domain.enums.StockStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Getter
+@NoArgsConstructor
+public class BookUpdateRequest {
+
+    @NotBlank(message = "도서 제목은 필수입니다.")
+    private String title;
+
+    @NotBlank(message = "출판사는 필수입니다.")
+    private String publisherName;
+
+    @NotNull(message = "카테고리는 필수입니다.")
+    private Long categoryId;
+
+    @NotBlank(message = "저자는 필수입니다.")
+    private String authorList;
+
+    @NotNull(message = "판매 상태는 필수입니다.")
+    private StockStatus stockStatus;
+
+    @Min(0)
+    private Integer priceStandard;
+
+    @Min(0)
+    private Integer priceSales;
+
+    @Min(0)
+    private Integer stock;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate publishedDate;
+
+    private String contributorList;
+    private String tagList;
+    private String subtitle;
+    private String bookIndex;
+    private String description;
+
+    private Integer pageCount;
+    private String language;
+    private String volumeNumber;
+    private Boolean packagingAvailable;
+
+}
