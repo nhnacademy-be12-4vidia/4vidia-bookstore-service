0a99ff5808c42890c127aedde583265b8f6b434c
Link <mexlove2@gmail.com>
2025-12-01 19:19:54 +0900

    feat: ES config 생성 - Elasticsearch config 생성

---
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/config/ElasticsearchConfig.java
@@ -0,0 +1,30 @@
+package com.nhnacademy._vidiabookstoreservice.book.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.nhnacademy._vidiabookstoreservice.book.repository.search")
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String esUrl;
+
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+            .connectedTo(esUrl)
+            .withBasicAuth(username, password)
+            .withSocketTimeout(30000)
+            .build();
+    }
+}
