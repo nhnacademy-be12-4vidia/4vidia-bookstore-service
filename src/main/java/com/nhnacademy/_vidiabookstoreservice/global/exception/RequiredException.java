51a3ccc0549ee53fd888b73970e490b0cc4d9ec7
Link <mexlove2@gmail.com>
2025-12-02 13:42:21 +0900

    feat: 예외 추가 - 커스텀 예외 추가

---
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/global/exception/RequiredException.java
@@ -0,0 +1,8 @@
+package com.nhnacademy._vidiabookstoreservice.global.exception;
+
+public class RequiredException extends RuntimeException {
+
+    public RequiredException(String message) {
+        super(message);
+    }
+}
