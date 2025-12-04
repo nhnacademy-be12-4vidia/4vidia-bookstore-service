e8adcfa1f502364666d0002a5291cad1a2683c46
jungboX <stonesys123@naver.com>
2025-12-04 14:13:00 +0900

    feat: 주문시 도서 재고차감

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/order/service/impl/OrderServiceImpl.java
@@ -68,9 +68,6 @@ public class OrderServiceImpl implements OrderService {
             user = userService.getUserById(userId);
         }
 
-        //TODO 재고 차감 구현 wow 어떻게하냐
-
-
         Order order = Order.builder()
                 .user(user)
                 .recipientName(request.recipientName())
@@ -89,6 +86,9 @@ public class OrderServiceImpl implements OrderService {
         Order savedOrder = orderRepository.save(order);
 
         for (OrderCreateRequest.ItemRequestDto itemDto : request.orderItems()) {
+
+            bookService.decreaseStock(itemDto.bookId(), itemDto.quantity());
+
             Book book = bookService.getBookEntity(itemDto.bookId());
 
             OrderItem orderItem = OrderItem.builder()
@@ -163,8 +163,14 @@ public class OrderServiceImpl implements OrderService {
     public OrderCheckoutResponse getOrderCheckoutResponse(Long userId, List<OrderCheckoutRequest> orderCheckoutRequests) {
 
         OrderUserResponse orderUserResponse = null;
-        if (userId != null) {
+        List<OrderPageCouponResponse> orderPageCouponResponses = null;
+        if (userId != null) { //회원인경우
             orderUserResponse = userService.getOrderUser(userId);
+
+            //TODO 쿠폰 연결하면 가져오기
+//        CouponRequest couponRequest = new CouponRequest(finalAmount, bookIds, categoryIds);
+//        orderPageCouponResponses = couponClient.getUserCoupons(couponRequest);
+
         } else {
             orderUserResponse = new OrderUserResponse("", "", "", 0, null);
         }
@@ -204,14 +210,9 @@ public class OrderServiceImpl implements OrderService {
                 .map(BookOrderResponse::category)
                 .toList();
 
-//        CouponRequest couponRequest = new CouponRequest(finalAmount, bookIds, categoryIds);
-//        List<OrderPageCouponResponse> orderPageCouponResponses = couponClient.getUserCoupons(couponRequest);
-
         List<DeliveryDateResponse> deliveryDateResponses = getDeliveryDates();
         List<PackagingOptionResponse> packagingOptions =  packagingOptionService.getPackagingOptions();
 
-
-
-        return OrderCheckoutResponse.from(orderUserResponse, bookItems, orderName, finalAmount, /*orderPageCouponResponses*/null, deliveryDateResponses, packagingOptions);
+        return OrderCheckoutResponse.from(orderUserResponse, bookItems, orderName, finalAmount, orderPageCouponResponses, deliveryDateResponses, packagingOptions);
     }
 }
