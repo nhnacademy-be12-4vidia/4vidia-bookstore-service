79a074bf88da391c1339a38f11a48507d8f0ab93
jungboX <stonesys123@naver.com>
2025-12-05 12:55:27 +0900

    feat: 주문생성 dto 추가

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/order/dto/order/request/OrderCreateRequest.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/order/dto/order/request/OrderCreateRequest.java
@@ -21,7 +21,6 @@ public record OrderCreateRequest(
         int payPrice, //도서가격 + 배송비 + 포장비 - 할인/포인트
 
         List<ItemRequestDto> orderItems,
-        //TODO 사용하는 쿠폰 아이디 리스트
         List<Long> coupons
 ) {
     public record ItemRequestDto(
