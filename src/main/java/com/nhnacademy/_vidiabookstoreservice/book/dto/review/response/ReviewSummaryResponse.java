24f1d16a88cf58c5795ddee2694eb0b8db6f39cc
Link <link@Linkui-MacBookPro.local>
2025-12-14 18:25:03 +0900

    feat: 요약된 리뷰 가져오기 위한 Dto 생성 - 요약된 리뷰 가져오는 Dto 생성 및 구현 - 기존 도서 상세페이지 응답에 요약리뷰 필드 추가

---
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/book/dto/review/response/ReviewSummaryResponse.java
@@ -0,0 +1,19 @@
+package com.nhnacademy._vidiabookstoreservice.book.dto.review.response;
+
+import com.nhnacademy._vidiabookstoreservice.book.domain.BookReviewSummary;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ReviewSummaryResponse {
+    private Long bookId;
+    private String summarizedReview;
+
+    public static ReviewSummaryResponse of(Long bookId, String summarizedReview) {
+        return ReviewSummaryResponse.builder()
+                .bookId(bookId)
+                .summarizedReview(summarizedReview)
+                .build();
+    }
+}
