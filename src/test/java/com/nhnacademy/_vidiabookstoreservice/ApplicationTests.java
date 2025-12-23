ea83ca0769ec2c413d7d649d167e476a1df1173b
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-12-23 12:47:12 +0900

    fix: 테스트 실패한거 수정, test.yml삭제 (test패키지에 추가해야함)

--- src/test/java/com/nhnacademy/_vidiabookstoreservice/ApplicationTests.java
+++ src/test/java/com/nhnacademy/_vidiabookstoreservice/ApplicationTests.java
@@ -1,16 +1,9 @@
 package com.nhnacademy._vidiabookstoreservice;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-@SpringBootTest
-@ActiveProfiles("local")
-class ApplicationTests {
+class ApplicationTests extends SupportControllerTest {
+
 
     @Test
     void contextLoads() {
