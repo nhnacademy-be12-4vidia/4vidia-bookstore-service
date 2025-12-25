5e5435740eeeb2c60cf6464dcd7bfb187a0b3e92
BaeJongOk <0511jongok@gmail.com>
2025-12-26 08:39:37 +0900

    fix: MDC import 변경

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/global/interceptor/LogInterceptor.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/global/interceptor/LogInterceptor.java
@@ -2,7 +2,7 @@ package com.nhnacademy._vidiabookstoreservice.global.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.jboss.logging.MDC;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
