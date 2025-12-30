9bc1fcec349c3f301176ecad47db1898f85dbe68
urim <kesnss55@gmail.com>
2025-12-30 12:45:55 +0900

    fix: 회원 등급 조회페이지 관련 코드 수정
    -

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/user/service/GradeService.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/user/service/GradeService.java
@@ -1,10 +1,14 @@
 package com.nhnacademy._vidiabookstoreservice.user.service;
 
 
+import com.nhnacademy._vidiabookstoreservice.user.dto.grade.response.GradePolicyResponse;
 import com.nhnacademy._vidiabookstoreservice.user.dto.grade.response.GradeResponse;
+
+import java.util.List;
 
 public interface GradeService {
     GradeResponse getGrade(Long userId);
     void updateGrade(Long userId, Long gradeId/*Grade grade*/);
     int recalculateMonthlyGrades();
+    List<GradePolicyResponse> getGradePolicies();
 }
